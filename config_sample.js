c89890b5006cfb3810eaa214ff5309eb03cfe4bc
SylvesterFrat <sylvesterfratarcangeli@gmail.com>
2023-01-09 18:22:12 -0500

    Original Version

---
+++ config_sample.js
@@ -0,0 +1,2 @@
+const MY_API_KEY = 'YOUR API KEY HERE';
+export {MY_API_KEY};
---
+++ converter.js
@@ -0,0 +1,44 @@
+import {MY_API_KEY} from './config.js';
+
+
+document.getElementById("convertButton").onclick = function() {
+    var amount = document.getElementById("amount").value;
+    console.log(amount);
+
+
+    var array = [];
+    dropdowns.forEach(dropdown => {
+        array.push(dropdown.querySelector('.selected').innerText);
+    })
+
+    console.log(array);
+    let from = array[0];
+    let to = array[1];
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': MY_API_KEY,
+            'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
+        }
+    };
+
+    let fetchLine = "https://currency-exchange.p.rapidapi.com/exchange?from=" + from + "&to=" + to + "&q=1.0";
+
+    if (from.localeCompare(to) == 0) { //doesnt work
+        console.log("Value", document.getElementById('amount').value)
+        document.getElementById('answer').innerHTML = document.getElementById('amount').value;
+    } else {
+        fetch(fetchLine, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log("Rate", response);
+            console.log("Value", document.getElementById('amount').value * response);
+            document.getElementById('answer').innerHTML = Math.round(((document.getElementById('amount').value * response) + Number.EPSILON) * 100) / 100 + " " + to;
+        })
+        .catch(err => console.error(err));
+    }
+}
+
+
+
